e95c610cc160337fc7cda6af539939f7719d9d98
Aymen Mehai <aymehaidev@protonmail.com>
2021-01-11 16:32:33 -0500

    added ghost valid moves test

--- Projects/P1/tests/TestGhostValidMoves.java
+++ Projects/P1/tests/TestGhostValidMoves.java
@@ -5,6 +5,9 @@ import java.io.*;
 public class TestGhostValidMoves extends TestCase {
 
 	public void testGhostValidMoves() throws FileNotFoundException{
-		return null;
+		NoFrame frame = new NoFrame();
+		Ghost newGhost = frame.addGhost(new Location(5, 5), "newGhost", Color.red);
+
+		assertTrue(newGhost.get_valid_moves().size() == 4);
 	}
 }
