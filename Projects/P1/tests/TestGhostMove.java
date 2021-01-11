11d4f3544662cd7122df4952ae29d377f9a71ca6
Leonel Santos <leonel.santos.a@gmail.com>
2021-01-10 21:02:52 -0500

    added a test for the ghost move method

--- Projects/P1/tests/TestGhostMove.java
+++ Projects/P1/tests/TestGhostMove.java
@@ -5,6 +5,11 @@ import java.io.*;
 public class TestGhostMove extends TestCase {
 
 	public void testGhostMove() throws FileNotFoundException{
-	
+		NoFrame frame = new NoFrame();
+
+		Ghost blinky = frame.addGhost(new Location(2, 2), "Blinky", Color.red);
+		asserTrue(blinky.move() == true)
+		Ghost bad_blinky = frame.addGhost(new Location(50, 50), "Bad Blinky", Color.red);
+		asserTrue(blinky.move() == false)
 	}
 }
