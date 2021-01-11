aa84a39c35771c9659acfbe041104d12b2a564b3
yairnbar <yairnbar@gmail.com>
2021-01-10 21:08:03 -0500

    added unit tests for mapEatCookie

--- Projects/P1/tests/TestMapEatCookie.java
+++ Projects/P1/tests/TestMapEatCookie.java
@@ -5,6 +5,22 @@ import java.io.*;
 public class TestMapEatCookie {
 	
 	public void testMapEatCookie() {
-		return null;
+		//Creating A Map 
+		MainFrame frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
+
+		//Creating Players
+		PacMan pacman = frame.addPacMan(new Location(0, 0)); //Creates PacMan at location x, y
+
+		//alternatively if you don't need the PacMan or Ghost objects in your tests
+		frame.initPlayers(); //Creates all of the players
+
+		//Start The Game
+		frame.startGame();
+
+		assertTrue(frame.getMap().eatCookie("pacman") == null);
+
+		pacman.myLoc.shift(1, 1);
+		
+		assertFalse(frame.getMap().eatCookie("pacman") == null);
 	}
 }
