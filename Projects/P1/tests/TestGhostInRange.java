4a9fe48cd1d571149c466e194109f6a6abbe9a4b
yairnbar <yairnbar@gmail.com>
2021-01-10 20:54:28 -0500

    added unit test for is_pacman_in_range

--- Projects/P1/tests/TestGhostInRange.java
+++ Projects/P1/tests/TestGhostInRange.java
@@ -5,7 +5,22 @@ import java.io.*;
 public class TestGhostInRange extends TestCase {
 
 	public void testGhostInRange() throws FileNotFoundException{
-		return null;
+		//Creating A Map 
+		Mainframe frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
+
+		//Creating Players
+		Ghost ghost = frame.addGhost(new Location(1, 0), "name", Color.red); //Creates a red ghost named "name" at location x,y
+		Ghost ghost2 = frame.addGhost(new Location(4, 4), "name", Color.blue); //Creates a blue ghost named "name" at location x,y
+		PacMan pacman = frame.addPacMan(new Location(0, 0)); //Creates PacMan at location x, y
+
+		//alternatively if you don't need the PacMan or Ghost objects in your tests
+		frame.initPlayers(); //Creates all of the players
+
+		//Start The Game
+		frame.startGame();
+
+		AssertTrue(ghost.is_pacman_in_range());
+		AssertFalse(ghost2.is_pacman_in_range());
 	}
 }
 
