df14edd875c89815db267226dc4c8cb1ec89198d
Leonel Santos <leonel.santos.a@gmail.com>
2021-01-11 15:55:25 -0500

    test for map attack method

--- Projects/P1/tests/TestMapAttack.java
+++ Projects/P1/tests/TestMapAttack.java
@@ -4,6 +4,13 @@ import junit.framework.*;
 public class TestMapAttack {
 
 	public void testMapAttack() {
-		return null;	
+		NoFrame frame = new NoFrame();
+		
+		frame.addPacMan(new Location(4, 4));
+		frame.addGhost(new Location(4, 5), "test", Color.white);
+		
+		assertTrue(frame.getMap().attack("test"));
+
+		assertTrue(frame.getMap().isGameOver());	
 	}
 }
