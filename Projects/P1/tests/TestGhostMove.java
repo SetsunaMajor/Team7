6bb526477361d3527105a8b198cad5166307ba56
Leonel Santos <leonel.santos.a@gmail.com>
2021-01-10 21:05:24 -0500

    correcting ghost location

--- Projects/P1/tests/TestGhostMove.java
+++ Projects/P1/tests/TestGhostMove.java
@@ -7,7 +7,7 @@ public class TestGhostMove extends TestCase {
 	public void testGhostMove() throws FileNotFoundException{
 		NoFrame frame = new NoFrame();
 
-		Ghost blinky = frame.addGhost(new Location(2, 2), "Blinky", Color.red);
+		Ghost blinky = frame.addGhost(new Location(1, 1), "Blinky", Color.red);
 		asserTrue(blinky.move() == true)
 		Ghost bad_blinky = frame.addGhost(new Location(50, 50), "Bad Blinky", Color.red);
 		asserTrue(blinky.move() == false)
