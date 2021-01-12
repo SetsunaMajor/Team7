e0dcba7fdd4ec87ecadd29827779176e4e58b997
Leonel Santos <leonel.santos.a@gmail.com>
2021-01-11 21:36:07 -0500

    fixing JUnit test issues

--- Projects/P1/tests/TestMapEatCookie.java
+++ Projects/P1/tests/TestMapEatCookie.java
@@ -1,12 +1,13 @@
-import junit.framework.*;
 import java.awt.Color;
+import junit.framework.*;
 import java.io.*;
+import static org.junit.Assert.*;
 
-public class TestMapEatCookie {
+public class TestMapEatCookie extends TestCase {
 	
-	public void testMapEatCookie() {
+	public void testMapEatCookie() throws FileNotFoundException {
 		//Creating A Map 
-		MainFrame frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
+		NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens Initialized
 
 		//Creating Players
 		PacMan pacman = frame.addPacMan(new Location(0, 0)); //Creates PacMan at location x, y
