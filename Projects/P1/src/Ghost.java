2855d6a44552a36b1cc06b51e5c451b96189f4f4
Leonel Santos <76961945+leonelasantos@users.noreply.github.com>
2021-01-11 19:20:12 -0500

    Update Ghost.java

--- Projects/P1/src/Ghost.java
+++ Projects/P1/src/Ghost.java
@@ -39,7 +39,7 @@ public class Ghost{
         }
 
         Random r = new Random();
-        int random_loc = r.nextInt(valid_moves.size());
+        int random_loc = r.nextInt(valid_moves.size() - 1);
 	myLoc = valid_moves.get(random_loc);
         myMap.move(myName, myLoc, Map.Type.GHOST);
 		
