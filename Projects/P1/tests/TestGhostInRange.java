9cc4675705c56051b99796ddf668d19f1abb6b2d
yairnbar <yairnbar@gmail.com>
2021-01-11 21:48:13 -0500

    fixed unit test

--- Projects/P1/tests/TestGhostInRange.java
+++ Projects/P1/tests/TestGhostInRange.java
@@ -1,6 +1,7 @@
 import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
+import static org.junit.Assert.*;
 
 public class TestGhostInRange extends TestCase {
 
@@ -12,12 +13,6 @@ public class TestGhostInRange extends TestCase {
 		Ghost ghost = frame.addGhost(new Location(1, 0), "name", Color.red); //Creates a red ghost named "name" at location x,y
 		Ghost ghost2 = frame.addGhost(new Location(4, 4), "name", Color.blue); //Creates a blue ghost named "name" at location x,y
 		PacMan pacman = frame.addPacMan(new Location(0, 0)); //Creates PacMan at location x, y
-
-		//alternatively if you don't need the PacMan or Ghost objects in your tests
-		frame.initPlayers(); //Creates all of the players
-
-		//Start The Game
-		frame.startGame();
 
 		assertTrue(ghost.is_pacman_in_range());
 		assertFalse(ghost2.is_pacman_in_range());
