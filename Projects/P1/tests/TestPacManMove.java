fbcd2f4d20f826a6e741fddc1092e017986d6c52
yairnbar <yairnbar@gmail.com>
2021-01-11 20:48:47 -0500

    fixed test fully

--- Projects/P1/tests/TestPacManMove.java
+++ Projects/P1/tests/TestPacManMove.java
@@ -6,21 +6,12 @@ public class TestPacManMove extends TestCase {
 
 	public void testPacManMove() throws FileNotFoundException{
 		//Creating A Map 
-		Mainframe frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
+		MainFrame frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
 
 		//Creating Players
-		PacMan pacman = frame.addPacMan(new Location(0, 0)); //Creates PacMan at location x, y
-
-		//alternatively if you don't need the PacMan or Ghost objects in your tests
-		frame.initPlayers(); //Creates all of the players
-
-		//Start The Game
-		frame.startGame();
-
+		PacMan pacman = frame.addPacMan(new Location(1, 1)); //Creates PacMan at location x, y
 		assertTrue(pacman.move());
-
-		pacman.myLoc.shift(50, 50);
-
-		assertFalse(pacman.move());
+		PacMan pacman2 = frame.addPacMan(new Location(50,50));
+		assertFalse(pacman2.move());
 	}
 }
