fb538537ab38d9afad5ae0d7d2d8465c81caf53f
Zachary Santamaria <zsantamaria@protonmail.com>
2021-01-11 22:35:22 -0500

    Adding fixes to the map eat cookie.

--- Projects/P1/tests/TestMapEatCookie.java
+++ Projects/P1/tests/TestMapEatCookie.java
@@ -7,22 +7,12 @@ import static org.junit.Assert.*;
 public class TestMapEatCookie extends TestCase {
 	
 	public void testMapEatCookie() throws FileNotFoundException {
-		//Creating A Map 
-		MainFrame frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
-
-		//Creating Players
-		PacMan pacman = frame.addPacMan(new Location(0, 0)); //Creates PacMan at location x, y
-
-		//alternatively if you don't need the PacMan or Ghost objects in your tests
-		frame.initPlayers(); //Creates all of the players
-
-		//Start The Game
+		MainFrame frame = new MainFrame();
+		PacMan pacman = frame.addPacMan(new Location(0, 0)); 
+		frame.initPlayers(); 
 		frame.startGame();
-
 		assertTrue(frame.getMap().eatCookie("pacman") == null);
-
 		pacman.myLoc.shift(1, 1);
-		
 		assertFalse(frame.getMap().eatCookie("pacman") == null);
 	}
 }
