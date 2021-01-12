f185c9980deb1fa9da8c204bacbdc42a9611cacc
Zachary Santamaria <zsantamaria@protonmail.com>
2021-01-11 22:47:06 -0500

    Fixing a typo

--- Projects/P1/tests/TestMapEatCookie.java
+++ Projects/P1/tests/TestMapEatCookie.java
@@ -7,12 +7,17 @@ import static org.junit.Assert.*;
 public class TestMapEatCookie extends TestCase {
 	
 	public void testMapEatCookie() throws FileNotFoundException {
-		MainFrame frame = new MainFrame();
-		PacMan pacman = frame.addPacMan(new Location(0, 0)); 
-		frame.initPlayers(); 
-		frame.startGame();
+
+        NoFrame frame = new NoFrame();
+
+		PacMan p1 = frame.addPacMan(new Location(0,0));
 		assertTrue(frame.getMap().eatCookie("pacman") == null);
-		pacman.myLoc.shift(1, 1);
-		assertFalse(frame.getMap().eatCookie("pacman") == null);
+
+		PacMan p2 = frame.addPacMan(new Location(1,1));
+		assertTrue(frame.getMap().eatCookie("pacman") != null);
+
+
+
+
 	}
 }
