bc14bcb6e7dd1568b16b3790ada64af7031d0d6e
rogerluo-zyz <61128507+rogerluo-zyz@users.noreply.github.com>
2021-01-18 10:51:45 +0800

    Update PacMan.java

--- Projects/P1/src/PacMan.java
+++ Projects/P1/src/PacMan.java
@@ -2,7 +2,7 @@ import java.util.HashSet;
 import java.util.Random;
 import java.util.ArrayList;
 import javax.swing.JComponent;
-//fix mark for push
+//Run tests
 public class PacMan{
 	String myName;
 	Location myLoc;
